381c84f3f77716da18857cadb1ca2acda05f6758
Hidayat Trimarsanto <anto@eijkman.go.id>
2018-03-23 10:11:39 +0700

    added commands

--- cmsfix/lib/cmds.py
+++ cmsfix/lib/cmds.py
@@ -4,6 +4,7 @@
 from rhombus.lib.utils import get_dbhandler
 from cmsfix.models.node import Node
 
+
 def get_node(arg):
 	""" get a node from arg """
 	if isinstance(arg, Node):
@@ -28,10 +29,27 @@ def ls(a_node):
 	for n in a_node.children:
 		print('%04d  %s' % (n.id, n.path))
 
+
+def add(a_node, new_node):
+	""" add new_node to a_node """
+	pass
+
+
+def update(a_node, data):
+	""" update a_node with data (either a dict or a node) """
+	prev_yaml = a_node.as_yaml()
+	a_node.update(data)
+	curr_yaml = a_node.as_yaml()
+	# create a diff from prev_yaml -> curr_yaml
+
+
 def mv(a_node, dest_node):
 	""" move a_node to dest_node """
 	pass
 
+
 def rm(a_node, opts=None):
 	""" remove a_node, recursively if needed """
 	pass
+
+# end of file
