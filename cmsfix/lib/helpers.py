03856034d9b808fac4a6d9e2de70d4fd81aa7266
Hidayat Trimarsanto <anto@eijkman.go.id>
2018-04-12 19:42:41 +0700

    added a function

--- cmsfix/lib/helpers.py
+++ cmsfix/lib/helpers.py
@@ -1,3 +1,10 @@
 
 from rhombus.lib.helpers import *
 
+def get_inherited_node(node, slug):
+	""" find a child node with slug in this node or consecutive parent nodes,
+		otherwise return None
+	"""
+
+	# need to think better strategies
+	pass
