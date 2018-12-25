e66cd02f7fca27c7ed57fff983d2adcdd2ca6376
Hidayat Trimarsanto <anto@eijkman.go.id>
2018-12-26 06:25:34 +0700

    add get_site()

--- cmsfix/views/__init__.py
+++ cmsfix/views/__init__.py
@@ -4,3 +4,16 @@ from pyramid.httpexceptions import HTTPFound
 
 from rhombus.views.generics import *
 from rhombus.lib.tags import *
+
+
+def get_site(request):
+
+	fqdn = request.registry.settings.get('cmsfix.site', None)
+
+    # if cmsfix.site is None, then do not use site information
+    # if cmsfix.site is *, then use hostname 
+    if fqdn == '*':
+        fqdn = request.host
+
+    return fqdn
+
