f9122c7081e5dae1155fc5aad5ca919243cadbe6
Hidayat Trimarsanto <anto@eijkman.go.id>
2021-03-11 19:05:05 +0700

    provide functional dashboard page

--- cmsfix/views/dashboard.py
+++ cmsfix/views/dashboard.py
@@ -3,12 +3,52 @@
 import os
 
 from rhombus.views import *
+from cmsfix.lib.workflow import get_workflow
 from cmsfix.views.node.pagenode import render_rst
 from pyramid.response import FileResponse
 
 @roles(PUBLIC)
 def index(request):
-    pass
+    """ return a generic dashboard """
+
+    html = div()
+    html.add( h2('Dashboard') )
+
+    html.add( h4('Latest updated pages') )
+
+    dbh = get_dbhandler()
+    nodes = dbh.session().query(dbh.PageNode).order_by(dbh.PageNode.stamp.desc())
+
+    accessible_nodes = []
+    limit = 10
+    counter = 0
+
+    for n in nodes:
+        if get_workflow(n).is_accessible(n, request):
+            accessible_nodes.append(n)
+            counter += 1
+            if counter > limit:
+                break
+
+    for node in accessible_nodes:
+        html.add(
+            div(class_='row')[
+                div(class_='col-md-1')[
+                    node.stamp.date(),
+                ],
+                div(class_='col-md-1')[
+                    node.lastuser.login,
+                ],
+                div(class_='col-md-10')[
+                    node.parent.title if node.parent else '', ' >> ', a(node.title or 'N/A', href=node.url),
+                    '|', node.path,
+                ]
+            ]
+        )
+
+    return render_to_response('cmsfix:templates/node/generics.mako',
+                { 'content': html,
+                }, request = request )
 
 
 @roles(PUBLIC)
